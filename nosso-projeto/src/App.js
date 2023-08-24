0b6a9e7af71b8821083e0ed0071f4370daef7dce
unknown <luisrocksousa1111@gmail.com>
2023-08-24 14:31:25 -0300

    Instalado e configurado o Taileind CSS #17

--- nosso-projeto/src/App.js
+++ nosso-projeto/src/App.js
@@ -8,6 +8,8 @@ function App() {
      <h1>Ola Pessoal</h1>
      <p>Vamos pegar o Nosso arquivo HTML e colocar aqui :)</p>
      <a href="#" className="btn btn-primary">testando a biblioteca bootstrap</a>
+     <br/>
+     <button className="bg-blue-500 px-3 rounded text-white hover:bg-blue-800">testando tailwind CSS</button>
     </div>
   );
 }
