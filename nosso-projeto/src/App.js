6c4fb381b7a2ab33c8284617a2b9fdcc72aede12
unknown <luisrocksousa1111@gmail.com>
2023-08-24 12:54:38 -0300

    instalado o Bootstrap no nosso-projeto #16

--- nosso-projeto/src/App.js
+++ nosso-projeto/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
      <h1>Ola Pessoal</h1>
-     <p>Vamos pegar o Nosso arquivo HTML aqui :)</p>
+     <p>Vamos pegar o Nosso arquivo HTML e colocar aqui :)</p>
+     <a href="#" className="btn btn-primary">testando a biblioteca bootstrap</a>
     </div>
   );
 }
