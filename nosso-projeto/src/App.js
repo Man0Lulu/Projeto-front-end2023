b516e2c4a3ec066907d54dd38a4898c278abb28d
unknown <luisrocksousa1111@gmail.com>
2023-08-24 11:40:03 -0300

    Convertendo o projeto em React #15

---
+++ nosso-projeto/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Ola Pessoal</h1>
+     <p>Vamos pegar o Nosso arquivo HTML aqui :)</p>
+    </div>
+  );
+}
+
+export default App;
