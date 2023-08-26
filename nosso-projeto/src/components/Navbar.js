e3275e0bdc29cef433ea3ca5d8b9493dba63672d
kimberly sousa <kimberlyamanda300@gmail.com>
2023-08-26 20:45:31 -0300

    alteração no nome da barra de navegação e alguns botões #20

--- nosso-projeto/src/components/Navbar.js
+++ nosso-projeto/src/components/Navbar.js
@@ -3,22 +3,22 @@ function Navbar() {
         <div>
             <nav className="navbar bg-body-tertiary fixed-top">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Offcanvas navbar</a>
+                    <a className="navbar-brand" href="#">Logo marca</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                         <div className="offcanvas-header">
-                            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+                            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Logo</h5>
                             <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                         </div>
                         <div className="offcanvas-body">
                             <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
                                 <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                    <a className="nav-link active" aria-current="page" href="#">Inicio</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">Link</a>
+                                    <a className="nav-link" href="#">Filmes/Desenhos</a>
                                 </li>
                                 <li className="nav-item dropdown">
                                     <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
